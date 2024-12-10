c36159316f8f00b5545bdef373e82a7490a37127
techhunt22 <techhunt22@gmail.com>
2024-12-11 00:23:06 +0500

    Vendor Details Screen Desgined

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Avatar = (): JSX.Element | null => {
   return (
-    <div className="avatar w-[214px] h-[70px] bg-white shadow-lg flex gap-2 items-center px-4 rounded-lg ">
+    <div className="avatar w-[215px] h-[70px] bg-white shadow-lg flex gap-2 items-center px-4 rounded-lg ">
       <Image
         src={"/images/avatar.png"}
         width={58}
